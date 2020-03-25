168befd05a3d9a20260f631594a669335e8dc066
Silva Muzime <muzimesilva@gmail.com>
2020-03-25 11:25:35 +0200

    Updates

---
+++ precache-manifest.aaef1cd5c5873f1d3e14bb1526edccd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f2ce71f794b6b594cfcae37902b9a8",
+    "url": "/SilvaMuzime/deploy-react-app/index.html"
+  },
+  {
+    "revision": "f84877d38bea921f4911",
+    "url": "/SilvaMuzime/deploy-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03b00b3445522da6287c",
+    "url": "/SilvaMuzime/deploy-react-app/static/js/2.ddafb193.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SilvaMuzime/deploy-react-app/static/js/2.ddafb193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f84877d38bea921f4911",
+    "url": "/SilvaMuzime/deploy-react-app/static/js/main.14106d6e.chunk.js"
+  },
+  {
+    "revision": "ab4cafb21cc6c3b5416b",
+    "url": "/SilvaMuzime/deploy-react-app/static/js/runtime-main.11476832.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SilvaMuzime/deploy-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
